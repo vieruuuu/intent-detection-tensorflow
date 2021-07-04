801b65d8d39d620a2282716963a8c1afeac2694c
vieru <mixerandr3w001@gmail.com>
2021-07-04 21:24:25 +0300

    fixed a typo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { train } = require("./lib/train.js");
-const run = require("./lib/run.js.js");
+const run = require("./lib/run.js");
 const load = require("./lib/load.js");
 
 async function main() {
   //let model = await train("Model");
   let model = await load("Model");
 
-  let prediction = await run(model, "tell me your age");
+  let prediction = await run(model, "please tell me your age");
   console.log(prediction);
 }
 
