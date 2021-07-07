9dcf316ae1df1d691d942d0b3095b1dc15965647
vieru <mixerandr3w001@gmail.com>
2021-07-07 10:53:01 +0300

    added more examples

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const run = require("./lib/run.js");
 const load = require("./lib/load.js");
 
 async function main() {
-  //let model = await train("Model");
-  let model = await load("Model");
+  let model = await train("Model");
+  //let model = await load("Model");
 
-  let prediction = await run(model, "turn on the christmas lights");
+  let prediction = await run(model, "turn the lights back off");
   console.log(prediction);
 }
 
