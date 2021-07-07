452254a84b91f1706653dd34b50b85b36d1310e5
vieru <mixerandr3w001@gmail.com>
2021-07-07 09:33:08 +0300

    commented the train func

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const run = require("./lib/run.js");
 const load = require("./lib/load.js");
 
 async function main() {
-  let model = await train("Model");
-  //let model = await load("Model");
+  //let model = await train("Model");
+  let model = await load("Model");
 
   let prediction = await run(model, "turn on the christmas lights");
   console.log(prediction);
